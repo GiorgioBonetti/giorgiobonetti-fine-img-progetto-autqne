a4482f5ae63807b71f132ffea9147b90defde111
GiorgioBonetti <giorgio.bonetti@marconirovereto.it>
2021-07-24 23:18:27 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { ElencopiattiService } from './common/elencopiatti.service';
 import { OrdineComponent } from './ordine/ordine.component';
 import { OridiniamoService } from './common/oridiniamo.service';
 import { ErroreComponent } from './errore/errore.component';
-import { AngularFireModule } from 'angularfire2';
+// import { AngularFireModule } from 'angularfire2';
 
 const LISTA_ROUTES = [
   { path: '', component: LoginComponent },
@@ -23,23 +23,23 @@ const LISTA_ROUTES = [
   { path: '**', component: ErroreComponent }
 ];
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCQw4MkPA-J5DHaBdfFDCEHrtumdDWGF8o',
-  authDomain: 'prova-e4cf7.firebaseapp.com',
-  databaseURL: 'https://prova-e4cf7-default-rtdb.firebaseio.com',
-  projectId: 'prova-e4cf7',
-  storageBucket: 'prova-e4cf7.appspot.com',
-  messagingSenderId: '498971305844',
-  appId: '1:498971305844:web:d5d270d93dd135fe836e21',
-  measurementId: 'G-MSGFFCT0C4'
-};
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyCQw4MkPA-J5DHaBdfFDCEHrtumdDWGF8o',
+//   authDomain: 'prova-e4cf7.firebaseapp.com',
+//   databaseURL: 'https://prova-e4cf7-default-rtdb.firebaseio.com',
+//   projectId: 'prova-e4cf7',
+//   storageBucket: 'prova-e4cf7.appspot.com',
+//   messagingSenderId: '498971305844',
+//   appId: '1:498971305844:web:d5d270d93dd135fe836e21',
+//   measurementId: 'G-MSGFFCT0C4'
+// };
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot(LISTA_ROUTES),
-    AngularFireModule.initializeApp(firebaseConfig)
+    // AngularFireModule.initializeApp(firebaseConfig)
   ],
   declarations: [
     AppComponent,
